228c87eb0f175aebcc17f87f1b5bf9f064cfa612
Ricardo Furbino <ricardofurbino@gmail.com>
2020-07-31 14:37:05 -0300

    Correção gráficos consolidados regionais

---
+++ Scripts/dateType.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df1 = pd.read_csv('file.csv',sep=';',encoding='latin-1')
+
+df1['Data'] = pd.to_datetime(df1['Data'], format='%m/%Y')
+
+df1.set_index('Data', inplace = True)
+df1.to_csv("file.csv", sep=';', quotechar='"', decimal='.', encoding='utf-8')
