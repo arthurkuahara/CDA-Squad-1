8bc1f44c0babfac233dc75e9a181e41a4b3153a3
LucasEAlves <lucaseliasalves.a@gmail.com>
2020-08-06 03:04:02 -0300

    Delete scripts

--- Dados Tratados/Região Nordeste/Pernambuco/dateType.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('pernambuco.csv',sep=';',encoding='latin-1')
-
-df1['Data'] = pd.to_datetime(df1['Data'], format='%m/%Y')
-
-df1.set_index('Data', inplace = True)
-df1.to_csv("pernambuco.csv", sep=';', quotechar='"', decimal='.', encoding='utf-8')
