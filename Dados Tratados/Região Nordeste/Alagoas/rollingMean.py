3b0934a6a3c9735fa0d32d2658649ef11462a2ad
LucasEAlves <lucaseliasalves.a@gmail.com>
2020-08-06 01:37:22 -0300

    Add Alagoas

---
+++ Dados Tratados/Região Nordeste/Alagoas/rollingMean.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import glob
+
+files = glob.glob('alagoasCredito.csv')
+
+for file in files:
+    df = pd.read_csv(file, sep=';', index_col=0)
+
+    for column in df:
+        df[column] = df[column].rolling(12, center=True, min_periods=1).mean()
+        df[column] = df[column].round(2)
+
+    filename = file[:-4] + "-media.csv"
+
+    df.to_csv(filename, sep=';', quotechar='"', decimal='.', encoding='utf-8')
